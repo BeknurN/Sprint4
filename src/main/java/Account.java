b3013cd4730ab5172694775c2a53717aa1c10b7b
BeknurN <beknur.narmukhan@kaspi.kz>
2022-06-21 11:39:36 +0600

    Com1

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,9 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
 public class Account {
 
     private final String name;
 
     public Account(String name) {
         this.name = name;
+
     }
 
     public boolean checkNameToEmboss() {
@@ -11,6 +13,12 @@ public class Account {
              Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
              Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
          */
+//        boolean containsValidSpaces = name.startsWith(" ") || name.endsWith(" ");
+//        //проверка на длинну
+//        boolean correctLength = name.length() >= 3 && name.length() <= 19;
+//        return !containsValidSpaces && correctLength;
+        int spaceCount = StringUtils.countMatches(name, " ");
+        return name.length() >= 3 && name.length() <= 19 && spaceCount == 1 && !(name.startsWith(" ") | name.endsWith(" "));
     }
 
 }
---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class AccountTest {
+
+    private final String name;
+    private final boolean expectedResult;
+
+    public AccountTest(String name, boolean expectedResult) {
+        this.name = name;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameters
+    public static Object[][] getParameters() {
+        return new Object[][]{
+                {"Нармуханбекнур", false},//0 пробелов
+                {"Нармухан Бекнур", true},//1 пробел
+                {"Первый нармухан бекнур", false},//2 пробела
+                {" Бекнур", false},//пробел в начале
+                {"Бекнур ", false},//пробел в конце
+                {" Бекнур ", false},//пробелы и в начале и в конце
+                {"Нармухан Бекнур", true},//пробел в середине
+                {" Нармухан Бекнур ", false},//пробелы в начале, в середине и в конце
+                {"Бе", false},//2 символа
+                {"Б н", true},//3 символа
+                {"Нармухан Бекнураааа", true},//19 символа
+                {"Нармухаааааан Бекнур", false},//20 символа
+        };
+    }
+
+    @Test
+    public void spaceTest() {
+        assertEquals(name, new Account(name).checkNameToEmboss(), expectedResult);
+    }
+}
